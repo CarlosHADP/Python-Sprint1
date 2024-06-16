bec9a9220717a88b76efe046abee7be8b5947aaf
Carlos Henrique Alves <carloshadp@gmail.com>
2024-06-16 01:08:38 -0300

    Finalizando o codigo

---
+++ FormulaE_Manager.py
@@ -0,0 +1,4 @@
+import random 
+
+# print("EQUIPES: \nMAHINDRA RACING \nABT CUPRA \nANDRETTI \nDS PENSKE \nENVISION RACING \nERT FORMULA \nJAGUAR TCS \nMASERATI MSG \nNEOM MCLAREN \nNISSAN \nTAG HEUER PORSCHE")
+equipe = str(input("Escolha uma equipe das opcoes dadas :").lower())
